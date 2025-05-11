2563bcf9b4058b0c93dab9a92082fba757236e5b
Axel1022 <ingcampusano@outlock.com>
2025-05-10 21:26:59 -0400

    GC - 10 MAY - CHORE - Eliminada ruta comentada DELETE /deleteStatus/:id del router de status

--- routers/status/statusRouter.js
+++ routers/status/statusRouter.js
@@ -4,6 +4,6 @@ const router = express.Router();
 import statusController from "../../controllers/status/statusController.js";
 router.get("/getStatus", statusController.getStatus);
 router.post("/postStatus", statusController.postStatus);
-router.delete("/deleteStatus/:id", statusController.deleteStatus);
+// router.delete("/deleteStatus/:id", statusController.deleteStatus); //Desactivada/no usar
 
 export default router;
