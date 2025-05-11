7440c6b264902979f395c57e750c9222072b925a
Axel1022 <ingcampusano@outlock.com>
2025-05-10 21:32:38 -0400

    GC - 10 MAY - FEAT - Creado modelo Sequelize para deudaColmaderoCliente

---
+++ models/deudaColmaderoCliente.js
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize";
+import db from "../config/db.js";
+
+const DeudaColmaderoCliente = db.define("deudaColmaderoCliente", {
+  uuid: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  uuidColmadero: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  totalMes: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  totalDeuda: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
+export default DeudaColmaderoCliente;
