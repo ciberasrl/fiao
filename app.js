32aee944319e2ed248f0b376eadaaa75a4917d4f
Axel1022 <ingcampusano@outlock.com>
2025-05-10 21:21:04 -0400

    GC - 10 MAY - CHORE - Agregado cookie-parser como middleware para solucionar error de ejecución

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import NotFund from "./controllers/404.js";
 import statusRouter from "./routers/status/statusRouter.js";
 import colmaderoRouter from "./routers/colmaderos/colmaderoRouter.js";
@@ -9,10 +10,11 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// Middleware
 app.use("/api/status", statusRouter);
 app.use("/api/colmadero", colmaderoRouter);
 app.use("/api/cliente", clienteRouter);
