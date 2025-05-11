1b602be90ab88199f431b3178226f72deccae6b6
Axel1022 <ingcampusano@outlock.com>
2025-05-10 21:26:21 -0400

    GC - 10 MAY - FEAT - Agregada ruta GET /getNameColmadero en el router de colmaderos

--- routers/colmaderos/colmaderoRouter.js
+++ routers/colmaderos/colmaderoRouter.js
@@ -2,12 +2,13 @@ import express from "express";
 const router = express.Router();
 
 import colmaderosController from "../../controllers/colmadero/colmaderoController.js";
-router.get("/getColmaderos", colmaderosController.getColmadero);
+router.get("/getColmaderos", colmaderosController.getColmaderos);
 router.post("/postColmadero", colmaderosController.postColmadero);
 router.put("/putNameColmadero", colmaderosController.updateNameColmadero);
 router.post(
   "/postActivarColmadero/:uuid",
   colmaderosController.postActivarColmadero
 );
+router.get("/getNameColmadero", colmaderosController.getNameColmadero);
 
 export default router;
